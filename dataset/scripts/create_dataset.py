280060eea98b272ed6b7d397095beb2708167669
Francesco Grillea <grilleafrancesco@gmail.com>
2022-12-09 18:03:27 +0100

    create dataset functions

--- dataset/scripts/create_dataset.py
+++ dataset/scripts/create_dataset.py
@@ -1,98 +1,83 @@
 import cv2
 import mediapipe as mp
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 import pandas as pd
 from utils import *
-import os
 
 
-user = "daniele"
-gesture = "thumbUp"
+def create_dataset(root_filepath):
+    df = pd.DataFrame()
 
-path = f'../{user}/'
-image_path = path + 'images/'
-print(os.listdir())
+    # TODO - browse recursively each video in folders
+    #df = pd.concat([df, record_stream(filename, frame_rate)], ignore_index=True)
 
-if not os.path.exists(path):
-    os.mkdir(path)
-
-if not os.path.exists(image_path):
-    print(image_path)
-    os.mkdir(image_path)
+    df.to_csv("dataset.csv")
 
 
-df = pd.DataFrame()
 
-cap = cv2.VideoCapture(0)
+def record_stream(filename, frame_rate, save_df=False):
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=1,
-                      min_detection_confidence=0.5,
-                      min_tracking_confidence=0.5)
-mpDraw = mp.solutions.drawing_utils
+    df = pd.DataFrame()
 
-current_frame = 5
-# Show results every x frame
-sleep_frame = 10
-
-frame_counter = 0
-
-while True:
-    _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-
-    results = hands.process(frame)
-
-    dictionary = {}
-
-    if results.multi_hand_landmarks:
-        landmarks = []
-        handLms = results.multi_hand_landmarks[0]
-
-        for enu, lm in enumerate(handLms.landmark):
-            landmark_xyz = [lm.x, lm.y, lm.z]
-            landmarks.append(landmark_xyz)
-
-        landmarks = normalize(landmarks)
-        dictionary = create_dict(landmarks)
-
-        if current_frame > sleep_frame:
-            current_frame = 0
-
-            gesture_path = image_path + gesture + "/"
-            if not os.path.exists(gesture_path):
-                os.mkdir(gesture_path)
-
-            filename = gesture_path + f"{frame_counter}.jpg"
-            print(filename)
-            cv2.imwrite(filename, frame)
-
-            frame_counter += 1
-
-            df = pd.concat([df, pd.DataFrame([dictionary])], ignore_index=True)
-            print(landmarks)
-
-        mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
-
-    current_frame += 1
-    print(current_frame)
-
-    # exit condition
-    cv2.imshow("Image", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# release the webcam and destroy all active windows
-cap.release()
-cv2.destroyAllWindows()
+    cap = cv2.VideoCapture(filename)
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False,
+                          max_num_hands=1,
+                          min_detection_confidence=0.5,
+                          min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
 
 
-# print(df)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        results = hands.process(frame)
 
-df.to_csv(path + gesture + ".csv")
+        if not ret:
+            break
 
 
+        # Show results every x seconds
+        # TODO - use frame_rate
+        sleep_time = 0.5
+        if datetime.now() > now + timedelta(0, sleep_time):
+            dictionary = {}
+
+            if results.multi_hand_landmarks:
+                landmarks = []
+                handLms = results.multi_hand_landmarks[0]
+
+                for enu, lm in enumerate(handLms.landmark):
+                    landmark_xyz = [lm.x, lm.y, lm.z]
+                    landmarks.append(landmark_xyz)
+
+                # normalize landmarks
+                landmarks = normalize(landmarks)
+                dictionary = create_dict(landmarks)
+                mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+                df = pd.concat([df, pd.DataFrame([dictionary])], ignore_index=True)
+                print(landmarks)
+
+            now = datetime.now()
+
+        #exit condition
+        cv2.imshow("Image", frame)
+
+    # release the webcam and destroy all active windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print(df)
+    if save_df:
+        df.to_csv(filename[:-3]+".csv")
+    return df
+
+
+
+
+
+# TODO - add execution
+#create_dataset(root_filepath)
 
 
 
